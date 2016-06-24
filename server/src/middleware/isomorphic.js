946db15e30f50650cd37307a1b0db51af7e1ae86
Tony Jiang <white87332@gmail.com>
2016-06-24 17:49:18 +0800

    fix head tag

--- server/src/middleware/isomorphic.js
+++ server/src/middleware/isomorphic.js
@@ -101,7 +101,7 @@ function renderFullPage(html, initialState, i18nClient)
     return (
         `<!doctype html>
         <html lang="utf-8">
-          <html lang="en">
+          <head>
               <meta charset="utf-8">
               <meta http-equiv="X-UA-Compatible" content="IE=edge">
               <meta name="viewport" content="width=device-width, initial-scale=1">
